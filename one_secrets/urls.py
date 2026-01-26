8fd4b840b89438845723040ab905efa280d855d0
Nazarov Artem <artemngetz@gmail.com>
2026-01-26 21:59:24 +0300

    object_detail and first url

---
+++ one_secrets/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+#связывает адрес в браузере с функцией/классом view
+
+#secret_url
+urlpatterns = [
+    path('<uuid:uuid>/', views.object_detail, name='object_detail'),
+]
--- one_secrets/views.py
+++ one_secrets/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+# view - получает запрос, обращается к модели, возвращает ответ
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseGone
+from one_secrets.models import Secret
+
+
+def object_detail(request, uuid):
+    # Получаем объект или возвращаем 404
+    obj = get_object_or_404(Secret, uuid=uuid)
+
+    # Вызываем доменный метод (например, read_once)
+    secret_text = obj.read_once()
+
+    # Возвращаем шаблон с контекстом
+    if secret_text is None:
+        return HttpResponseGone("Секрет уничтожен или не существует")
+    else:
+        return render(request, 'secret_detail.html', {'secret_text': secret_text})
