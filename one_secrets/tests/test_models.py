e74bda32333501aba6e1eb09b0008d96f01c9dba
Nazarov Artem <artemngetz@gmail.com>
2026-01-31 20:23:38 +0300

    expired, transaction tests

--- one_secrets/tests/test_models.py
+++ one_secrets/tests/test_models.py
@@ -2,7 +2,6 @@ import pytest
 from django.test import TestCase
 from django.utils import timezone
 from datetime import timedelta, datetime
-
 from one_secrets.models import Secret
 
 FUTURE = timezone.make_aware(datetime(2099, 1, 1))
@@ -22,3 +21,25 @@ def test_read_once_returns_none_on_second_call():
     res = secret.read_once()
     res = secret.read_once()
     assert res is None
+
+
+@pytest.mark.django_db
+def test_read_once_race():
+    """Тест: если два клиента одновременно пытаются прочитать одноразовый секрет"""
+    secret = Secret.objects.create(secret="TopSecret", expires_at=FUTURE)
+    first_result = secret.read_once()
+    second_result = secret.read_once()
+    assert first_result == "TopSecret"
+    assert second_result is None
+
+
+MIN_LATER = timezone.now() - timedelta(minutes=10)
+
+
+@pytest.mark.django_db  # Разрешить доступ к БД
+def test_expired():
+    """Тест: истёк срок секрета"""
+    secret = Secret.objects.create(secret="TopSecret", expires_at=MIN_LATER)
+    result = secret.read_once()
+
+    assert result is None
---
+++ one_secrets/tests/test_views.py
@@ -0,0 +1,5 @@
+import pytest
+from django.test import TestCase
+from django.utils import timezone
+from datetime import timedelta, datetime
+from one_secrets.models import Secret
