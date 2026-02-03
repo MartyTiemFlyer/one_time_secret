63e921f353acc43755b53ee2ad5b8ad30baf6b09
Nazarov Artem <artemngetz@gmail.com>
2026-02-03 20:47:44 +0300

    admin update read_only_field

--- one_secrets/admin.py
+++ one_secrets/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from one_secrets.models import Secret
+
 
 # Register your models here.
-from one_secrets.models import Secret
-admin.site.register(Secret)
+
+@admin.register(Secret)
+class SecretAdmin(admin.ModelAdmin):
+    """Секрет — иммутабельный, после создания его нельзя менять даже из админки"""
+
+    list_display = ("uuid",)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # объект уже существует --> change view
+            return ("secret",)
+        return ()
